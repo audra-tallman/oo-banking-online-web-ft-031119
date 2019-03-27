71fbd174c77ddceb39f3a185c7563d5565fd9458
Audra Tallman <audi.tallman@gmail.com>
2019-03-27 03:40:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -5,7 +5,7 @@ class Transfer
   def initialize(sender, receiver, transfer_amount)
     @sender= sender
     @receiver= receiver
-    @transfer_amount= transfer_amount
+    @transfer_amount= num
     @status= "pending"
   end 
 
