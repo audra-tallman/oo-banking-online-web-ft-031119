5049f80325f07030f0c5d5e00005ea7682af0cdb
Audra Tallman <audi.tallman@gmail.com>
2019-03-27 03:55:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end 
   
   def execute_transaction
-    if self.valid? 
+    if self.valid? and sender.amount < balance
      "Transaction successful."
     else 
       "Transaction rejected. Please check your account balance."
